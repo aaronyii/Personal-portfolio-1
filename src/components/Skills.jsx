8cf1ca4cb8c6e638d819b0b4efc4cb2fa673c2b6
Aaron Yii <aaronyii@mail.usf.edu>
2023-07-22 20:41:18 -0500

    Added Work Component

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,7 +3,6 @@ import CSS from '../assets/css.png';
 import JavaScript from '../assets/javascript.png';
 import ReactImg from '../assets/react.png';
 import Node from '../assets/node.png';
-import FireBase from '../assets/firebase.png';
 import GitHub from '../assets/github.png';
 import Tailwind from '../assets/tailwind.png';
 import Mongo from '../assets/mongo.png';
@@ -20,35 +19,35 @@ const Skills = () => {
             <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
                 <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                     <img src={HTML} alt="HTML icon" className='w-20 mx-auto'/>
-                    <p className='my-4'>HTML</p>
+                    <p className='my-4 font-bold'>HTML</p>
                 </div>
                 <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                     <img src={CSS} alt="CSS icon" className='w-20 mx-auto'/>
-                    <p className='my-4'>CSS</p>
+                    <p className='my-4 font-bold'>CSS</p>
                 </div>
                 <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                     <img src={JavaScript} alt="JavaScript icon" className='w-20 mx-auto'/>
-                    <p className='my-4'>JavaScript</p>
+                    <p className='my-4 font-bold'>JavaScript</p>
                 </div>
                 <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                     <img src={ReactImg} alt="React icon" className='w-20 mx-auto'/>
-                    <p className='my-4'>React</p>
+                    <p className='my-4 font-bold'>React</p>
                 </div>
                 <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                     <img src={Node} alt="Node icon" className='w-20 mx-auto'/>
-                    <p className='my-4'>Node</p>
+                    <p className='my-4 font-bold'>Node</p>
                 </div>
                 <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                     <img src={Mongo} alt="Mongo icon" className='w-20 mx-auto'/>
-                    <p className='my-4'>Mongo</p>
+                    <p className='my-4 font-bold'>Mongo</p>
                 </div>
                 <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                     <img src={Tailwind} alt="Tailwind icon" className='w-20 mx-auto'/>
-                    <p className='my-4'>Tailwind</p>
+                    <p className='my-4 font-bold'>Tailwind</p>
                 </div>
                 <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                     <img src={GitHub} alt="GitHub icon" className='w-20 mx-auto'/>
-                    <p className='my-4'>GitHub</p>
+                    <p className='my-4 font-bold'>GitHub</p>
                 </div>
             </div>
         </div>
