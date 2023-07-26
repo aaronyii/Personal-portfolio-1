196f706d0d5ad6daa609b854619c50277eb0dc8b
Aaron Yii <aaronyii@mail.usf.edu>
2023-07-26 15:16:42 -0500

    Removed some projects from works component

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,4 +1,3 @@
-import WorkImg from '../assets/projects/workImg.jpeg'
 import realEstate from '../assets/projects/realestate.jpg'
 const Work= () => {
   return (
@@ -11,24 +10,6 @@ const Work= () => {
 {/* Container */}
         <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
           {/* Grid Items */}
-          <div style={{backgroundImage: `url(${WorkImg})`}} 
-          className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center 
-          items-center mx-auto content-div'>
-            {/* Hover Effects */}
-            <div className='opacity-0 group-hover:opacity-100'>
-              <span className='text-2xl font-bold text-white tracking-wider'>
-                React JS Application
-              </span>
-              <div className='pt-8 text-center'>
-                <a href="/">
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
-                </a>
-                <a href="/">
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
-                </a>
-              </div>
-            </div>
-          </div>
           <div style={{backgroundImage: `url(${realEstate})`}} 
           className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center 
           items-center mx-auto content-div'>
@@ -48,24 +29,6 @@ const Work= () => {
             </div>
           </div>
           {/* Grid Items */}
-          <div style={{backgroundImage: `url(${WorkImg})`}} 
-          className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center 
-          items-center mx-auto content-div'>
-            {/* Hover Effects */}
-            <div className='opacity-0 group-hover:opacity-100'>
-              <span className='text-2xl font-bold text-white tracking-wider'>
-                React JS Application
-              </span>
-              <div className='pt-8 text-center'>
-                <a href="/">
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
-                </a>
-                <a href="/">
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
-                </a>
-              </div>
-            </div>
-          </div>
           <div style={{backgroundImage: `url(${realEstate})`}} 
           className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center 
           items-center mx-auto content-div'>
@@ -85,24 +48,6 @@ const Work= () => {
             </div>
           </div>
           {/* Grid Items */}
-          <div style={{backgroundImage: `url(${WorkImg})`}} 
-          className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center 
-          items-center mx-auto content-div'>
-            {/* Hover Effects */}
-            <div className='opacity-0 group-hover:opacity-100'>
-              <span className='text-2xl font-bold text-white tracking-wider'>
-                React JS Application
-              </span>
-              <div className='pt-8 text-center'>
-                <a href="/">
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
-                </a>
-                <a href="/">
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
-                </a>
-              </div>
-            </div>
-          </div>
           <div style={{backgroundImage: `url(${realEstate})`}} 
           className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center 
           items-center mx-auto content-div'>
