de1aeb9beceaa01b19fd9809aaeb4528a55a58d1
Aaron Yii <aaronyii@mail.usf.edu>
2023-07-27 20:34:49 -0500

    Added new project to work component

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,67 +1,72 @@
-import realEstate from '../assets/projects/realestate.jpg'
-const Work= () => {
+import realEstate from "../assets/projects/realestate.jpg";
+import mealsApp from "../assets/projects/meals-app.png";
+const Work = () => {
   return (
-    <div name="work" className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
-      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
-        <div className='pb-8'>
-          <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>Work</p>
-          <p className='py-6 '>Check out my recent work</p>
+    <div name="work" className="w-full md:h-screen text-gray-300 bg-[#0a192f]">
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600">
+            Work
+          </p>
+          <p className="py-6 ">Check out my recent work</p>
         </div>
-{/* Container */}
-        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+        {/* Container */}
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
           {/* Grid Items */}
-          <div style={{backgroundImage: `url(${realEstate})`}} 
-          className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center 
-          items-center mx-auto content-div'>
+          <div
+            style={{ backgroundImage: `url(${mealsApp})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center 
+          items-center mx-auto content-div"
+          >
             {/* Hover Effects */}
-            <div className='opacity-0 group-hover:opacity-100'>
-              <span className='text-2xl font-bold text-white tracking-wider'>
-                React JS Application
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                React Meals App
               </span>
-              <div className='pt-8 text-center'>
-                <a href="/">
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+              <div className="pt-8 text-center">
+                <a
+                  href="https://meals-app-reactjs.netlify.app/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
                 </a>
-                <a href="/">
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                <a
+                  href="https://github.com/aaronyii/meals-app"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
                 </a>
               </div>
             </div>
           </div>
+          
           {/* Grid Items */}
-          <div style={{backgroundImage: `url(${realEstate})`}} 
-          className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center 
-          items-center mx-auto content-div'>
+          <div
+            style={{ backgroundImage: `url(${realEstate})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center 
+          items-center mx-auto content-div"
+          >
             {/* Hover Effects */}
-            <div className='opacity-0 group-hover:opacity-100'>
-              <span className='text-2xl font-bold text-white tracking-wider'>
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
                 React JS Application
               </span>
-              <div className='pt-8 text-center'>
+              <div className="pt-8 text-center">
                 <a href="/">
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
                 </a>
                 <a href="/">
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
-                </a>
-              </div>
-            </div>
-          </div>
-          {/* Grid Items */}
-          <div style={{backgroundImage: `url(${realEstate})`}} 
-          className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center 
-          items-center mx-auto content-div'>
-            {/* Hover Effects */}
-            <div className='opacity-0 group-hover:opacity-100'>
-              <span className='text-2xl font-bold text-white tracking-wider'>
-                React JS Application
-              </span>
-              <div className='pt-8 text-center'>
-                <a href="/">
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
-                </a>
-                <a href="/">
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
                 </a>
               </div>
             </div>
@@ -69,7 +74,7 @@ const Work= () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Work
+export default Work;
