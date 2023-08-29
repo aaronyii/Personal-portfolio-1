fd6d2c7985dc44644df2bf9d479f03bdeaadb5ed
Aaron Yii <aaronyii@mail.usf.edu>
2023-08-29 10:56:58 -0500

    Modified Email and Resume

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
           Aaron Yii
         </h1>
         <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
-          I'm a full stack developer.{" "}
+          I'm a full stack developer.
         </h2>
         <p className="text-[#8892b0] py-4 max-w-[700px]">
             Passionate about learning new things and am presently engaged in
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -122,7 +122,7 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href="mailto:aaronmanabc@gmail.com"
+              href="mailto:0710aaronyii@gmail.com"
             >
               Email <HiOutlineMail size={30} />
             </a>
